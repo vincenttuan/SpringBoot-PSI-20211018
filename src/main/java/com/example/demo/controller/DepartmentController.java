6a4bcaab3085b115424cfecb26637c186bd68e22
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-01 20:25:54 +0800

    Add files via upload

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,14 +1,28 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Department;
+import com.example.demo.repository.DepartmentRepository;
 
 @Controller
 @RequestMapping("/department")
 public class DepartmentController {
 	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		Department department = new Department();
+		List<Department> departments = departmentRepository.findAll();
+		model.addAttribute("department", department);
+		model.addAttribute("departments", departments);
 		return "department";
 	}
 	
