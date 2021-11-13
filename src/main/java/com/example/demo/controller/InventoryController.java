a1f3af49193757d8fd2e13dabdc13a0757ac28c7
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-13 22:58:00 +0800

    Update InventoryController.java

--- src/main/java/com/example/demo/controller/InventoryController.java
+++ src/main/java/com/example/demo/controller/InventoryController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/inventory")
 public class InventoryController {
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("investories", productRepository.queryInventory());
 		return "inventory";
 	}
 	
